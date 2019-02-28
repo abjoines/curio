7a9c1c1f40459769b9fd488109f9de5b9a3110bd
abjoines <joina778@newschool.edu>
2019-02-28 16:51:48 -0500

    ANDY ADDITION

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var container, stats;
-var camera, scene, renderer;
+var camera, scene, renderer, controls;
 var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var midX = window.innerWidth / 2;
+var midY = window.innerHeight / 2;
 
 init();
 animate();
@@ -11,20 +11,41 @@ function init() {
 
     container = document.createElement( 'div' );
     document.body.appendChild( container );
-    camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 200;
+
+    // renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
 
     // scene
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x8e8e8e );
-    var ambientLight = new THREE.AmbientLight( 0x404040 , 1 );
-    scene.add( ambientLight );
-    var pointLight = new THREE.PointLight( 0xffffff, 1 );
-    scene.add( ambientLight );
-    camera.add( pointLight );
+
+    // camera
+    camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.z = 200;
     scene.add( camera );
 
-    // model
+    // controls
+    // controls = new THREE.TrackballControls( camera );
+	// 			controls.rotateSpeed = 1.0;
+	// 			controls.zoomSpeed = 1.2;
+	// 			controls.panSpeed = 0.8;
+	// 			controls.noZoom = false;
+	// 			controls.noPan = false;
+	// 			controls.staticMoving = true;
+	// 			controls.dynamicDampingFactor = 0.3;
+
+    // lights
+    var ambientLight = new THREE.AmbientLight( 0x404040 , 1 );
+    scene.add( ambientLight );
+
+    var pointLight = new THREE.PointLight( 0xffffff, 0.5 );
+    scene.add( ambientLight );
+    camera.add( pointLight );
+
+    // models
     var onProgress = function ( xhr ) {
         if ( xhr.lengthComputable ) {
             var percentComplete = xhr.loaded / xhr.total * 100;
@@ -35,67 +56,74 @@ function init() {
     THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
     new THREE.MTLLoader()
         .setPath( './media/' )
-        .load( 'roomPreset.mtl', function ( materials ) {
+        .load( 'roomPresetTRY2.mtl', function ( materials ) {
             materials.preload();
             new THREE.OBJLoader()
                 .setMaterials( materials )
                 .setPath( './media/' )
-                .load( 'RoomPreset.obj', function ( object ) {
+                .load( 'RoomPresetTRY2.obj', function ( object ) {
                     object.position.y = - 25;
                     scene.add( object );
                 }, onProgress, onError );
         } );
 
-        THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-        new THREE.MTLLoader()
-            .setPath( './media/' )
-            .load( 'Alyssa2.mtl', function ( materials ) {
-                materials.preload();
-                new THREE.OBJLoader()
-                    .setMaterials( materials )
-                    .setPath( './media/' )
-                    .load( 'Alyssa2.obj', function ( object ) {
-                        object.position.y = - 30;
-                        object.rotateY(160);
-                        object.scale.set(2,2,2);
-                        scene.add( object );
-                    }, onProgress, onError );
-            } );
-    
-    // var loader = new THREE.FBXLoader();
-    // loader.load( '/media/Alyssa.fbx', function ( object ) {
-    // object.position.y = - 25;
-    // object.rotateY(90); 
-    // scene.add( object );
-    // } );
+    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+    new THREE.MTLLoader()
+        .setPath( './media/' )
+        .load( 'Alyssa2.mtl', function ( materials ) {
+            materials.preload();
+            new THREE.OBJLoader()
+                .setMaterials( materials )
+                .setPath( './media/' )
+                .load( 'Alyssa2.obj', function ( object ) {
+                    object.position.y = - 33.5;
+                    object.rotateY(160);
+                    object.scale.set(3,3,3);
+                    scene.add( object );
+                }, onProgress, onError );
+        } );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
+    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+    new THREE.MTLLoader()
+        .setPath( './media/' )
+        .load( 'Andy2.mtl', function ( materials ) {
+            materials.preload();
+            new THREE.OBJLoader()
+                .setMaterials( materials )
+                .setPath( './media/' )
+                .load( 'Andy2.obj', function ( object ) {
+                    object.position.y = - 34.5;
+                    object.position.x = -30;
+                    object.rotateY(180);
+                    object.scale.set(3,3,3);
+                    scene.add( object );
+                }, onProgress, onError );
+        } );
+
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    //
-
     window.addEventListener( 'resize', onWindowResize, false );
+
 }
+
 function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
+    midX = window.innerWidth / 2;
+    midY = window.innerHeight / 2;
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight - 100 );
 }
 function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
+    mouseX = ( event.clientX - midX ) / 2;
+    mouseY = ( event.clientY - midY ) / 2;
 }
-//
+
 
 function animate() {
     requestAnimationFrame( animate );
     render();
 }
 function render() {
+    // controls.update();
     camera.position.x += ( - mouseX/2 - camera.position.x ) * 0.5;
     camera.position.y += ( mouseY/2 - camera.position.y ) * 0.5;
     camera.lookAt( scene.position );
